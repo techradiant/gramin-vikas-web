6701cf7988723ad10d90576b2011b59981b88c64
vikasjangra029 <110277952+vikasjangra029@users.noreply.github.com>
2023-11-01 18:58:42 +0530

    Panchayat RithalPhogat Detail Added

---
+++ src/GraminVikas.Web/Controllers/Panchayat.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GraminVikas.Web.Controllers
+{
+    public class Panchayat : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult RithalNarwal()
+        {
+            return View();
+        }
+
+        public IActionResult RithalPhogat()
+        {
+            return View();
+        }
+    }
+}
