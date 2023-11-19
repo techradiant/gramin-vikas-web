f9296832aae0c346b85ad2456558fbaa89d7fce0
vikasjangra029 <110277952+vikasjangra029@users.noreply.github.com>
2023-11-19 15:01:24 +0530

    GramPanchayat Section updated

--- src/GraminVikas.Web/Data/ApplicationDbContext.cs
+++ src/GraminVikas.Web/Data/ApplicationDbContext.cs
@@ -1,13 +1,48 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GraminVikas.Web.Data
 {
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("UserClaims");
+            });
+          
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("UserRoles");
+            });
+           
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("UserLogins");
+            });
+          
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("Roles");
+            });
+        
+            modelBuilder.Entity<IdentityUserClaim<string>>(b =>
+            {
+                b.ToTable("Users");
+            });
+        }
+
     }
+
 }
